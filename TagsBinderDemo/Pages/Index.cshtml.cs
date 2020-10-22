6e2fd46d4dfdc04f10d63eb7ff1d6b10c9be891f
Hakim Ghods <ghods.hakim@hotmail.com>
2020-10-23 03:02:34 +0330

    Add project files.

---
+++ TagsBinderDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TagsBinderDemo.Models;
+
+namespace TagsBinderDemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public Video Video { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string[] Tags { get; set; }
+
+        public void OnPost()
+        {
+            Tags = Video.Tags.Split(",");
+
+        }
+    }
+}
